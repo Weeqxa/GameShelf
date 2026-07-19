9e96d3ed0182832db237e82f1d0ebf1e74b70004
weeqxa <weeqxa.69@gmail.com>
2026-07-19 22:16:20 +0200

    Created the RegisterResponse DTO for the response.

---
+++ src/main/java/com/weeqxa/gameshelf/dto/RegisterResponse.java
@@ -0,0 +1,13 @@
+package com.weeqxa.gameshelf.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RegisterResponse {
+
+    private String message;
+
+}
