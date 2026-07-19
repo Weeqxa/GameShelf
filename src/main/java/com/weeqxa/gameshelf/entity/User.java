6e7aea518d4a63cdfd6d4cd2a1e4df097e441152
weeqxa <weeqxa.69@gmail.com>
2026-07-19 22:12:32 +0200

    Added a condition for the username to be not null

--- src/main/java/com/weeqxa/gameshelf/entity/User.java
+++ src/main/java/com/weeqxa/gameshelf/entity/User.java
@@ -3,6 +3,7 @@ package com.weeqxa.gameshelf.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
 
@@ -23,14 +25,12 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "username")
+    @Column(name = "username", nullable = false)
     private String username;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    public User() {
-    }
 
     public User(String email, String password, String username) {
         this.email = email;
