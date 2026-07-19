9dbef48f41dcc594984ebd5cc41e1f83644c51c0
weeqxa <weeqxa.69@gmail.com>
2026-07-19 22:18:27 +0200

    Created AuthController and added a method that handles user registration requests.

---
+++ src/main/java/com/weeqxa/gameshelf/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.weeqxa.gameshelf.controller;
+
+
+import com.weeqxa.gameshelf.dto.RegisterRequest;
+import com.weeqxa.gameshelf.dto.RegisterResponse;
+import com.weeqxa.gameshelf.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@Valid @RequestBody RegisterRequest request) {
+        RegisterResponse response = authService.register(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+}
