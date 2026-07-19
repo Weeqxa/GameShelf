b8948bbec7f9d25676deaf090bce9ae249881e09
weeqxa <weeqxa.69@gmail.com>
2026-07-19 22:13:57 +0200

    Created UserRepository and added the existsByEmail method to it.

---
+++ src/main/java/com/weeqxa/gameshelf/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.weeqxa.gameshelf.repository;
+
+import com.weeqxa.gameshelf.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+}
