e355af9f2369447ccd69968b2e98c2b2b091b48b
weeqxa <weeqxa.69@gmail.com>
2026-07-20 21:53:42 +0200

    Add new findById method

--- src/main/java/com/weeqxa/gameshelf/repository/UserRepository.java
+++ src/main/java/com/weeqxa/gameshelf/repository/UserRepository.java
@@ -3,9 +3,13 @@ package com.weeqxa.gameshelf.repository;
 import com.weeqxa.gameshelf.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
-    User findByEmail(String email);
+    Optional<User> findById(Long id);
+
+    Optional<User> findByEmail(String email);
 }
