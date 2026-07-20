9ad5e983ff494ca39da7f6477c189a194058d80d
weeqxa <weeqxa.69@gmail.com>
2026-07-20 21:58:03 +0200

    Add LoginRequest DTO class

---
+++ src/main/java/com/weeqxa/gameshelf/dto/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.weeqxa.gameshelf.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is not valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 20)
+    private String password;
+
+
+}
