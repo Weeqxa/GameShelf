14231873e38c0447106537eba639bf8f05e8d059
weeqxa <weeqxa.69@gmail.com>
2026-07-20 21:59:30 +0200

    Implement CustomUserDetailsService class

---
+++ src/main/java/com/weeqxa/gameshelf/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.weeqxa.gameshelf.security;
+
+
+import com.weeqxa.gameshelf.repository.UserRepository;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@NullMarked
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return new CustomUserDetails(userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found")));
+    }
+}
