feaab5ac1f3275df8220d50b729e16ef2939cdcc
weeqxa <weeqxa.69@gmail.com>
2026-07-20 21:56:24 +0200

    Update SecurityConfig for JWT token and remove unnecessary method

--- src/main/java/com/weeqxa/gameshelf/config/SecurityConfig.java
+++ src/main/java/com/weeqxa/gameshelf/config/SecurityConfig.java
@@ -1,37 +1,40 @@
 package com.weeqxa.gameshelf.config;
 
 
+import com.weeqxa.gameshelf.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     //http://localhost:8080/swagger-ui.html
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+
         return http
-                .csrf(csrf -> csrf.disable()).
-                authorizeHttpRequests(auth -> auth.
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth.
                         requestMatchers("/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
-                                "/auth/**")
+                                "/auth/login", "/auth/register")
                         .permitAll()
                         .anyRequest()
-                        .authenticated()).build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+                        .authenticated())
+                .build();
     }
 
     @Bean
