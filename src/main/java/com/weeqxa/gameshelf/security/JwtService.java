6b85bfd93d56b29aa8274dd2525e7d503d1a244f
weeqxa <weeqxa.69@gmail.com>
2026-07-20 21:58:36 +0200

    Implement standard JwtService with 5 methods

---
+++ src/main/java/com/weeqxa/gameshelf/security/JwtService.java
@@ -0,0 +1,72 @@
+package com.weeqxa.gameshelf.security;
+
+
+import com.weeqxa.gameshelf.entity.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey;
+
+    private final long expiration;
+
+    public JwtService(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expiration
+    ) {
+
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        this.expiration = expiration;
+    }
+
+    public String generateToken(User user) {
+
+        return Jwts
+                .builder()
+                .subject(user.getEmail())
+                .claim("id", user.getId())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public Long extractUserId(String token) {
+        return Long
+                .valueOf(extractAllClaims(token).getId());
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+
+}
