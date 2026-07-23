f771c99f638f48262b0e0fb1f78fcb7c7fe04b72
weeqxa <weeqxa.69@gmail.com>
2026-07-23 21:42:33 +0200

    Add additional package (refactor imports)

--- src/main/java/com/weeqxa/gameshelf/service/GameService.java
+++ src/main/java/com/weeqxa/gameshelf/service/GameService.java
@@ -2,8 +2,8 @@ package com.weeqxa.gameshelf.service;
 
 
 import com.weeqxa.gameshelf.client.RawgClient;
-import com.weeqxa.gameshelf.dto.GameDto;
-import com.weeqxa.gameshelf.dto.GamesResponseDto;
+import com.weeqxa.gameshelf.dto.games.GameDto;
+import com.weeqxa.gameshelf.dto.games.GamesResponseDto;
 import com.weeqxa.gameshelf.dto.rawg.RawgGameDto;
 import org.springframework.stereotype.Service;
 
