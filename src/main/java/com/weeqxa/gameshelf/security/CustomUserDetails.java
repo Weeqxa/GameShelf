8a320cd2c5405c0064b67478dbb209b38f5be6e4
weeqxa <weeqxa.69@gmail.com>
2026-07-23 21:41:40 +0200

    Add getUserId method

--- src/main/java/com/weeqxa/gameshelf/security/CustomUserDetails.java
+++ src/main/java/com/weeqxa/gameshelf/security/CustomUserDetails.java
@@ -1,11 +1,13 @@
 package com.weeqxa.gameshelf.security;
 
 import com.weeqxa.gameshelf.entity.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+
 
 public class CustomUserDetails implements UserDetails {
 
@@ -30,6 +32,9 @@ public class CustomUserDetails implements UserDetails {
         return true;
     }
 
+    public Long getUserId() {
+        return user.getId();
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
